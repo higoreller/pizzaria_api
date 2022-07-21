f83454ba3bc9724a5b586fdca035c3e0b923a203
higoreller <higoreller29@gmail.com>
2022-07-20 22:57:19 -0300

    20/07/2022

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import {Router, Request, Response } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
 // User routes
 router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+
 
 export { router }; 
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,33 @@
+import prismaClient from "../../prisma";
+import {compare} from 'bcryptjs'
+
+interface AuthRequest{
+    email: string
+    password: string
+}
+
+class AuthUserService{
+    async execute({email, password}:AuthRequest){
+        //Verify if the email exists
+        const user = await prismaClient.user.findFirst({
+            where:{
+                email:email
+            }
+        })
+
+        if(!user){
+            throw new Error("User/password incorrect")
+        }
+
+        //Verify if the password is correct
+        const passwordMatch = await compare(password, user.password)
+
+        if(!passwordMatch){
+            throw new Error("User/password incorrect")
+        }
+        
+        return {ok: true}
+    }
+}
+
+export { AuthUserService}
